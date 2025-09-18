2435ddb30418b019b4a5f64725de4235d5c2a95f
Lê Khánh <lkdev.react@gmail.com>
2025-09-18 16:11:21 +0700

    commit

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -6,6 +6,7 @@ const Contact = () => {
     <div className=' mx-auto w-full mb-28 '>
         <div className='flex flex-col items-center justify-center mt-28'>
           <h1 className='text-3xl font-semibold'>LIÊN HỆ VỚI CHÚNG TÔI</h1>
+          <strong>(Cho xin địa chỉ cái bạn ơi)</strong>
           <div className='flex mt-10 '>
             <h3 className='text-xl font-semibold text-purple-700'>Partnership & Marketing:  </h3>
             <p className='ml-3 text-lg font-semibold'>devtoanbug@mkt.com,</p>
@@ -13,11 +14,11 @@ const Contact = () => {
           </div>
           <div className='flex mt-5'>
             <h3 className='text-xl font-semibold text-purple-700'>Pháp lý:  </h3>
-            <p className='ml-3 text-lg font-semibold'>devtoanbug@contact.com</p>
+            <p className='ml-3 text-lg font-semibold'>Gặp anh Khánh</p>
           </div>
           <div className='flex mt-5'>
             <h3 className='text-xl font-semibold text-purple-700'>Hỗ trợ:  </h3>
-            <p className='ml-3 text-lg font-semibold'>devtoanbug@support.com</p>
+            <p className='ml-3 text-lg font-semibold'>Gặp anh Bảo</p>
           </div>
           <div className='flex mt-5'>
             <h3 className='text-xl font-semibold text-purple-700'>Địa chỉ:  </h3>
@@ -25,7 +26,7 @@ const Contact = () => {
           </div>
           <div className='flex mt-5'>
             <h3 className='text-xl font-semibold text-purple-700'>Số điện thoại:  </h3>
-            <p className='ml-3 text-lg font-semibold'>1900 8098</p>
+            <p className='ml-3 text-lg font-semibold'>Đoán xem</p>
           </div>
           
         </div>
